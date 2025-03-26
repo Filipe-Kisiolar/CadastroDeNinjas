baff3ad06b08c1a230acdcfcb6e7511e30a2cb03
Filipe Kisiolar <filipekisiolarfilho@terra.com.br>
2025-03-26 18:55:18 -0300

    criado o NinjaModel,e a entidade no BD

--- src/main/java/dev/ninjascadastro/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/ninjascadastro/CadastroDeNinjas/Controller.java
@@ -1,2 +1,16 @@
-package dev.ninjascadastro.CadastroDeNinjas;public class Controller {
+package dev.ninjascadastro.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String BoasVindas() {
+        return "Essa e minha primeira mensagem";
+    }
 }
+
